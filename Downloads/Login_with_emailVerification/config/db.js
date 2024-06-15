72c9c3de09101526199d7091c59d6ca14c6a5256
avdhesh0009 <avdheshkumar41237@gmail.com>
2024-06-15 22:27:01 +0530

    Revert "first commit"
    This reverts commit 85b34da6381792dc56e384b2a89f14a8fd397053.

--- Downloads/Login_with_emailVerification/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-// Connect to database
-mongoose.connect(process.env.MONGODB_URL)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
