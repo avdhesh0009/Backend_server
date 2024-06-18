10efcf2c014efcca8fc55f230d32837adb49657e
avdhesh0009 <avdheshkumar41237@gmail.com>
2024-06-18 14:25:39 +0530

    first commit

--- Downloads/login_server/api/routes/User.js
+++ Downloads/login_server/api/routes/User.js
@@ -48,7 +48,7 @@ const sendVerificationEmail = ({_id,email},res)=>{
         to: email,
         subject: "Verify Your Email",
         html: `<p>Verify your email address to complete the signup and login into your account.</p><p>This link 
-        <b>expires in 6 hours</b>.</p><p>Press <a href=${
+        <b>expires in 2 min</b>.</p><p>Press <a href=${
             currentUrl + "user/verify/" + _id + "/" + uniqueString
         }>here</a> to proceed.</p>`
     }
@@ -61,7 +61,7 @@ const sendVerificationEmail = ({_id,email},res)=>{
         const newVerification = new UserVerification({
             userId:_id,
             uniqueString:hashedUniqueString,
-            expiresAt:Date.now() + 21600000,
+            expiresAt:Date.now() + 120000,
         })
 
         newVerification.save()
